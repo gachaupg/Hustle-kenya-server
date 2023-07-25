eca45aa398c1054d1d05213dbc810cacfbfde129
Ebenezer Okello <okelloebenezer403@gmail.com>
2023-07-25 19:52:21 +0300

    hello

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,29 @@
 import express from "express";
-import { loginUser, registerUser } from "../controllers/auth.js";
+import {
+  deleteUser,
+  getUser,
+  forgotPassword,
+  getUsers,
+  googleSignIn,
+  signin,
+  signup,
+  updateUser,
+  resetPassword,
+  postResetPassword,
+} from "../controllers/auth.js";
 
 const router = express.Router();
+// all user routes
 
-router.post("/register", registerUser);
-router.post("/login", loginUser);
+router.post("/register", signup);
+router.post("/login", signin);
+router.get("/all-users", getUsers);
+router.get("/user-profile/:id", getUser);
+router.delete("/delete-user/:id", deleteUser);
+router.patch("/update-user/:id", updateUser);
+router.post("/google-signin", googleSignIn);
+router.post("/forgot-password", forgotPassword);
+router.get("/reset-password/:id/:token", resetPassword);
+router.post("/reset-password/:id/:token", postResetPassword);
 
 export default router;
