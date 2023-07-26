49f728c46eaac227f6188a130a8b48c1e8d69c38
ndibomk <ndibomk@gmail.com>
2023-07-26 21:47:59 +0300

    commited

--- routes/Categories.js
+++ routes/Categories.js
@@ -63,3 +63,60 @@ router.get ('/live',  async (req,res)=>{
         res.status(500).send(error)
     }
 })
+router.get ('/news',  async (req,res)=>{
+    const previosMonth=moment()
+    .month(moment().month()-1)
+    .set('date',1)
+    .format('YYYY-MM-DD HH:mm:ss');
+    // res.status(200).send(previosMonth)
+    try {
+        const users= await userContent.aggregate([
+            {$match:{category:'news'},
+        
+        }
+       
+        ]);
+        res.status(200).send(users);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error)
+    }
+})
+router.get ('/motivation',  async (req,res)=>{
+    const previosMonth=moment()
+    .month(moment().month()-1)
+    .set('date',1)
+    .format('YYYY-MM-DD HH:mm:ss');
+    // res.status(200).send(previosMonth)
+    try {
+        const users= await userContent.aggregate([
+            {$match:{category:'motivation'},
+        
+        }
+       
+        ]);
+        res.status(200).send(users);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error)
+    }
+})
+router.get ('/comedy',  async (req,res)=>{
+    const previosMonth=moment()
+    .month(moment().month()-1)
+    .set('date',1)
+    .format('YYYY-MM-DD HH:mm:ss');
+    // res.status(200).send(previosMonth)
+    try {
+        const users= await userContent.aggregate([
+            {$match:{category:'comedy'},
+        
+        }
+       
+        ]);
+        res.status(200).send(users);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error)
+    }
+})
