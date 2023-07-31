a9e657281bc12b98cf1503717dd0491126833e98
ndibomk <ndibomk@gmail.com>
2023-07-31 11:52:19 +0300

    Commited

--- models/userContents.js
+++ models/userContents.js
@@ -1,35 +1,27 @@
 import mongoose from 'mongoose';
 const { ObjectId } = mongoose.Types;
 
-const userContentSchema=  mongoose.Schema({
-title:{type:String,required:true},
-    title: { type: String, required: true },
-    userName: { type: String},
-    vedeo: { type: String},
-    likes: { type: Number, default: 0 },
-    dislikes: { type: Number, default: 0 },
-    comments: [],
-    views: { type: Number, default: 0 },
-    description: { type: String },
-    playList:[],
-    creator:{type:String},
-    category:{type:String},
-    img:{type:String},
-    hashtags:{type:String},
-    userChanellLink: { type: String },
-    socialMediaLinks: { type: String },
-    playList: { type: String },
-    contactDetails: { type: String },
-    remarks: { type: String },
-    additionalInfo: { type: String },
-    date: { type: Date, default: new Date() },
-createdAt: {
-    type: Date,
-    default: new Date(),
-  },
-
-},
-{timestamps:true})
+const userContentSchema = mongoose.Schema({
+  title: { type: String, required: true },
+  userName: { type: String },
+  video: { type: String },
+  likes:[{type:ObjectId,ref:"Auth"}],
+  dislikes: [{type:ObjectId,ref:"Auth"}],
+  comments: [],
+  views: { type: Number, default: 0 },
+  description: { type: String },
+  playList: [],
+  creator: { type: String },
+  category: { type: String },
+  img: { type: String },
+  hashtags: { type: String },
+  userChannelLink: { type: String },
+  socialMediaLinks: { type: String },
+  contactDetails: { type: String },
+  remarks: { type: String },
+  additionalInfo: { type: String },
+  date: { type: Date, default: new Date() },
+}, { timestamps: true });
 
 const userContent = mongoose.model("userVedeoContents", userContentSchema);
 
